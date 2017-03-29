0136cf2ef91c5ae56778beccb29e9f8c2f8d8ab2
angel <angelyazarov@yahoo.com>
2017-03-29 22:57:50 +0100

    Updated Styles and UI, Templates, Engine and ToolManager

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,34 +1,7 @@
 class Engine {
     constructor() {
-        this._selectedColor = ControlsManager.colors[0];
-        this._selectedTool;
-        this._lineWidth;
 
-        ControlsManager
-            .initiateControls()
-            .attachEvents();
-
-        DrawingCanvas.init();
-    }
-
-    get selectedColor() {
-        return this._selectedColor;
-    }
-    set selectedColor(x) {
-        this._selectedColor = x
-    }
-    get selectedTool() {
-        return this._selectedTool;
-    }
-    set selectedTool(x) {
-        this._selectedTool = x
-    }
-    get lineWidth() {
-        return this._lineWidth;
-    }
-    set lineWidth(x) {
-        this._lineWidth = x
+        ToolsManager.initControls()
     }
 }
-
 let engine = new Engine();
